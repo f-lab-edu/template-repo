651c28e2c10929f7adad5b7a71f6c93ae6374348
Jaehyeon Han <jaehyeonhan99@gmail.com>
2026-05-02 23:45:30 +0900

    chore(app): 메시지 조회 Swagger 시간 예시 수정

--- app/src/main/java/io/github/jaehyeonhan/project/controller/dto/request/GetNewMessageRequest.java
+++ app/src/main/java/io/github/jaehyeonhan/project/controller/dto/request/GetNewMessageRequest.java
@@ -1,5 +1,6 @@
 package io.github.jaehyeonhan.project.controller.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,5 +9,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class GetNewMessageRequest {
     private final String userId;
+
+    @Schema(example = "2025-11-22T02:53:40.578")
     private final LocalDateTime lastRead;
 }
